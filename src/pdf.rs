98de30863142658cec9f3822dde6bcd5a38eaccf
Homayoun <hhio618@gmail.com>
2022-12-08 20:04:58 +0330

    Add static pdfium instance (#31)

--- src/pdf.rs
+++ src/pdf.rs
@@ -5,15 +5,18 @@ use std::{
 };
 
 use image::DynamicImage;
-
+use once_cell::sync::OnceCell;
 use pdfium_render::prelude::*;
+
+static PDFIUM: OnceCell<Pdfium> = OnceCell::new(); // static initializers must impl Sync + Send
 
 pub enum PDFQuality {
     High,
     Medium,
     Low,
 }
-fn initialize_pdfium() -> Box<dyn PdfiumLibraryBindings> {
+
+fn initialize_pdfium() {
     let out_path = env!("OUT_DIR");
     let pdfium_lib_path =
         PathBuf::from(&out_path).join(Pdfium::pdfium_platform_library_name());
@@ -23,15 +26,12 @@ fn initialize_pdfium() -> Box<dyn PdfiumLibraryBindings> {
         #[cfg(not(target_os = "android"))]
         pdfium_lib_path.to_str().unwrap(),
     )
-    .or_else(|_| Pdfium::bind_to_system_library());
-
-    match bindings {
-        Ok(binding) => binding,
-        Err(e) => {
-            panic!("{:?}", e)
-        }
-    }
+    .or_else(|_| Pdfium::bind_to_system_library())
+    .unwrap();
+    PDFIUM.set(Pdfium::new(bindings)); // Instead of returning the bindings, we
+                                       // cache them in the static initializer
 }
+
 pub fn render_preview_page<R>(data: R, quailty: PDFQuality) -> DynamicImage
 where
     R: Read + Seek + 'static,
@@ -43,7 +43,13 @@ where
         PDFQuality::Low => render_cfg.thumbnail(50),
     }
     .rotate_if_landscape(PdfBitmapRotation::Degrees90, true);
-    Pdfium::new(initialize_pdfium())
+
+    if PDFIUM.get().is_none() {
+        initialize_pdfium();
+    }
+    PDFIUM
+        .get()
+        .unwrap()
         .load_pdf_from_reader(data, None)
         .unwrap()
         .pages()
