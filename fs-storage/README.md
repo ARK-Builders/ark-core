8c28aa9d8f1774f89dbe715551ac1bdf69e87abf
Tarek <tareknaser360@gmail.com>
2024-03-29 09:02:14 +0200

    feat(fs-storage): refactor CLI write cmd to accept key-value pairs
    Signed-off-by: Tarek <tareknaser360@gmail.com>

--- fs-storage/README.md
+++ fs-storage/README.md
@@ -3,7 +3,9 @@
 File system storage implementation for writing key value pairs to disk.
 
 ## Steps to use CLI
+
 - Create a test.json file of key:values pairs you want to store.
+
 ```json
 {
   "key1": "value1",
@@ -13,22 +15,32 @@ File system storage implementation for writing key value pairs to disk.
 ```
 
 - Run Write Command
+
 ```bash
 cargo run --example cli write /tmp/z test.json
 ```
 
+Alternatively, you can directly provide the input data as a comma-separated list of key-value pairs
+
+```bash
+cargo run --example cli write /tmp/z a:1,b:2,c:3
+```
+
 - Run Read Command
+
 ```bash
 cargo run --example cli read /tmp/z key1,key2
 ```
 
 - Get Output
+
 ```bash
 key1: value1
 key2: value2
 ```
 
 - To get all key value pairs
+
 ```bash
 cargo run --example cli read /tmp/z
 ```
--- fs-storage/examples/cli.rs
+++ fs-storage/examples/cli.rs
@@ -16,7 +16,7 @@ fn run() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
         println!("Usage:");
-        println!(" cargo run --example cli write <path> <json_file>");
+        println!(" cargo run --example cli write <path> [JSON_FILE_PATH | KEY_VALUE_PAIRS]");
         println!(" cargo run --example cli read <path> <key1,key2,...>");
         return Ok(());
     }
@@ -66,31 +66,45 @@ fn read_command(args: &[String], path: &str) -> Result<()> {
 
 fn write_command(args: &[String], path: &str) -> Result<()> {
     if args.len() < 4 {
-        println!("Usage: cargo run -- write <path> <json_file>");
+        println!("Usage: cargo run --example cli write <path> [JSON_FILE_PATH | KEY_VALUE_PAIRS]");
         return Ok(());
     }
 
-    let json_file = &args[3];
-    let json_contents =
-        fs::read_to_string(json_file).context("Failed to read JSON file")?;
-    let json_value: Value =
-        serde_json::from_str(&json_contents).context("Invalid JSON")?;
+    let content = &args[3];
+    // Check if the content is a JSON file path
+    let content_json = Path::new(content)
+        .extension()
+        .map_or(false, |ext| ext == "json");
 
     let mut kv_pairs = BTreeMap::new();
-    if let Value::Object(object) = json_value {
-        for (key, value) in object {
-            if let Value::String(value_str) = value {
-                kv_pairs.insert(key, value_str);
-            } else {
-                println!(
-                    "Warning: Skipping non-string value for key '{}'",
-                    key
-                );
+    if content_json {
+        let content =
+            fs::read_to_string(content).context("Failed to read JSON file")?;
+        let json: Value =
+            serde_json::from_str(&content).context("Failed to parse JSON")?;
+        if let Value::Object(object) = json {
+            for (key, value) in object {
+                if let Value::String(value_str) = value {
+                    kv_pairs.insert(key, value_str);
+                } else {
+                    println!(
+                        "Warning: Skipping non-string value for key '{}'",
+                        key
+                    );
+                }
             }
+        } else {
+            println!("JSON value is not an object");
+            return Ok(());
         }
     } else {
-        println!("JSON value is not an object");
-        return Ok(());
+        let pairs = content.split(',');
+        for pair in pairs {
+            let kv: Vec<&str> = pair.split(':').collect();
+            if kv.len() == 2 {
+                kv_pairs.insert(kv[0].to_string(), kv[1].to_string());
+            }
+        }
     }
 
     let mut fs = FileStorage::new("cli".to_string(), Path::new(path));
