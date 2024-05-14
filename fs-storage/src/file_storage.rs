504060a7598936c2f486eb32af8bab4425abbc3c
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2024-05-14 17:08:31 +0530

    Manually convert BTreeMap to java LinkedHashMap

--- fs-storage/src/file_storage.rs
+++ fs-storage/src/file_storage.rs
@@ -347,6 +347,7 @@ mod tests {
     }
 }
 
+use jni::signature::{JavaType, ReturnType};
 // This is the interface to the JVM that we'll call the majority of our
 // methods on.
 use jni::JNIEnv;
@@ -354,12 +355,12 @@ use jni::JNIEnv;
 // These objects are what you should use as arguments to your native
 // function. They carry extra lifetime information to prevent them escaping
 // this context and getting used after being GC'd.
-use jni::objects::{JClass, JString};
+use jni::objects::{JClass, JObject, JString, JValue};
 
 // This is just a pointer. We'll be returning it from our function. We
 // can't return one of the objects with lifetime information because the
 // lifetime checker won't let us.
-use jni::sys::{jboolean, jlong, jobject};
+use jni::sys::{jboolean, jlong, jobject, jvalue};
 
 impl FileStorage<String, String> {
     fn from_jlong<'a>(value: jlong) -> &'a mut Self {
@@ -407,12 +408,12 @@ pub extern "system" fn Java_FileStorage_remove(
 
     FileStorage::from_jlong(file_storage_ptr)
         .remove(&id)
-        .unwrap();
+        .unwrap()
 }
 
 #[no_mangle]
 pub extern "system" fn Java_FileStorage_is_storage_updated(
-    env: &mut JNIEnv,
+    _env: &mut JNIEnv,
     _class: JClass,
     file_storage_ptr: jlong,
 ) -> jboolean {
@@ -428,15 +429,51 @@ pub extern "system" fn Java_FileStorage_read_fs(
     _class: JClass,
     file_storage_ptr: jlong,
 ) -> jobject {
-    let data = FileStorage::from_jlong(file_storage_ptr)
-        .read_fs()
+    let data: BTreeMap<String, String> =
+        FileStorage::from_jlong(file_storage_ptr)
+            .read_fs()
+            .unwrap();
+
+    // Create a new LinkedHashMap object
+    let linked_hash_map_class =
+        env.find_class("java/util/LinkedHashMap").unwrap();
+    let linked_hash_map = env
+        .new_object(linked_hash_map_class, "()V", &[])
         .unwrap();
-    todo!()
+
+    // Get the put method ID
+    let put_method_id = env
+        .get_method_id(
+            "java/util/LinkedHashMap",
+            "put",
+            "(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
+        )
+        .unwrap();
+
+    // Insert each key-value pair from the BTreeMap into the LinkedHashMap
+    for (key, value) in data {
+        let j_key = env.new_string(key).unwrap();
+        let j_value = env.new_string(value).unwrap();
+        let j_key = JValue::from(&j_key).as_jni();
+        let j_value = JValue::from(&j_value).as_jni();
+        unsafe {
+            env.call_method_unchecked(
+                &linked_hash_map,
+                put_method_id,
+                ReturnType::Object,
+                &[j_key, j_value],
+            )
+            .unwrap()
+        };
+    }
+
+    // Return the LinkedHashMap as a raw pointer
+    linked_hash_map.as_raw()
 }
 
 #[no_mangle]
 pub extern "system" fn Java_FileStorage_write_fs(
-    env: &mut JNIEnv,
+    _env: &mut JNIEnv,
     _class: JClass,
     file_storage_ptr: jlong,
 ) {
@@ -448,7 +485,7 @@ pub extern "system" fn Java_FileStorage_write_fs(
 ///! Safety: The FileStorage instance is dropped after this call
 #[no_mangle]
 pub extern "system" fn Java_FileStorage_erase(
-    env: &mut JNIEnv,
+    _env: &mut JNIEnv,
     _class: JClass,
     file_storage_ptr: jlong,
 ) {
