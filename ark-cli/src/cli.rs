abb29d00067f65eb63583cfa578845c9149ea928
Tarek <tareknaser360@gmail.com>
2024-05-24 23:01:12 +0300

    feat(ark-cli): add cli styles
    Signed-off-by: Tarek <tareknaser360@gmail.com>

--- ark-cli/src/cli.rs
+++ ark-cli/src/cli.rs
@@ -1,11 +1,18 @@
 use crate::commands::Commands;
 
-use clap::Parser;
+use clap::{builder::styling::AnsiColor, Parser};
 
 #[derive(Parser, Debug)]
 #[clap(name = "ark-cli")]
-#[clap(about = "Manage ARK tag storages and indexes", long_about = None)]
+#[clap(about = "Manage ARK tag storages and indexes", styles=styles())]
 pub struct Cli {
     #[clap(subcommand)]
     pub command: Commands,
+}
+
+pub fn styles() -> clap::builder::Styles {
+    clap::builder::Styles::styled()
+        .header(AnsiColor::Yellow.on_default())
+        .usage(AnsiColor::Yellow.on_default())
+        .literal(AnsiColor::Green.on_default())
 }
